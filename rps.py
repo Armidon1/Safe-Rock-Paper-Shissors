87c2e361fe34aa4225a5ba7b2d46919e4d007008
WhiteSteps <simo.nole12@gmail.com>
2025-12-03 00:01:17 +0100

    created a method for rock paper shissor with a pseudorandom functuion

---
+++ rps.py
@@ -0,0 +1,21 @@
+import secrets
+
+rps = {0:"Shissors", 1:"Rock", 2:"Paper"}
+
+def rock_paper_shissors_secure():
+    return rps[secrets.randbelow(3)]
+
+if __name__ == "__main__":
+    count_rps = {"Shissors":0, "Rock":0, "Paper":0}
+    print(("-"*10)+"Testing"+("-"*10))
+    for i in range(1000000000):
+        match rock_paper_shissors_secure():
+            case "Shissors":
+                count_rps["Shissors"] +=1
+            case "Rock":
+                count_rps["Rock"] +=1
+            case "Paper":
+                count_rps["Paper"] +=1
+    print(f"Shissors = {count_rps["Shissors"]}, Rock = {count_rps["Rock"]}, Paper = {count_rps["Paper"]}")
+    print(("-"*10)+"Finish"+("-"*10))
+    
